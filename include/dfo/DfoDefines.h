b2bc40027a73172227645a635f0f6254b9c11fb4
blackrebel <72fabio1@virgilio.it>
2018-05-22 19:23:16 +0200

    Add dfo headers folder

---
+++ include/dfo/DfoDefines.h
@@ -0,0 +1,15 @@
+#ifndef __DFO_DEFINES_H__
+#define __DFO_DEFINES_H__
+
+#define MTK_ENABLE_MD5 0
+#define MTK_DT_SUPPORT 0
+#define MTK_TELEPHONY_MODE 1
+#define MTK_SHARE_MODEM_SUPPORT 2
+#define MTK_SHARE_MODEM_CURRENT 2
+#define MTK_ENABLE_MD1 1
+#define MTK_ENABLE_MD2 0
+#define MTK_FIRST_MD 1
+#define MTK_CTA_SUPPORT 0
+#define MTK_AUTOIP_SUPPORT 0
+
+#endif
